9cd672c479d71c5b2d34ab5d58394fe6b34aa726
JoyC14 <40276392+JoyC14@users.noreply.github.com>
2021-03-19 15:08:01 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,55 @@
+let 骰子 = 0
+input.onGesture(Gesture.Shake, function () {
+    if (骰子 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (骰子 == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (骰子 == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (骰子 == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (骰子 == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+    骰子 = randint(1, 6)
+})
